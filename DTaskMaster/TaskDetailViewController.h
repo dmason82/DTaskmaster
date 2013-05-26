99bc966a0f95f27abe628fba35c9bbc7fe6c9d69
Doug Mason <d.mason82@gmail.com>
2013-05-25 22:08:15 -0700

    Starting to build out detail view controller.

---
+++ DTaskMaster/TaskDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  TaskDetailViewController.h
+//  DTaskMaster
+//
+//  Created by Doug Mason on 5/25/13.
+//  Copyright (c) 2013 Doug Mason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TaskDetailViewController : UIViewController <UIActionSheetDelegate>
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *actionButton;
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (weak, nonatomic) IBOutlet UITextField *taskTitleField;
+@property (weak, nonatomic) IBOutlet UITextView *taskDetailField;
+
+@end
---
+++ DTaskMaster/TaskDetailViewController.m
@@ -0,0 +1,45 @@
+//
+//  TaskDetailViewController.m
+//  DTaskMaster
+//
+//  Created by Doug Mason on 5/25/13.
+//  Copyright (c) 2013 Doug Mason. All rights reserved.
+//
+
+#import "TaskDetailViewController.h"
+
+@interface TaskDetailViewController ()
+
+@end
+
+@implementation TaskDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidUnload {
+    [self setActionButton:nil];
+    [self setDatePicker:nil];
+    [self setTaskTitleField:nil];
+    [self setTaskDetailField:nil];
+    [super viewDidUnload];
+}
+@end
